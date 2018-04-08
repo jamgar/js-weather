668fd437014c534a3470291d9a010ec418006bc3
github username <github email address>
2018-04-08 17:18:09 -0500

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+require('../node_modules/normalize.css/normalize.css')
 require('./css/style.css')
+import * as views from './views'
+
 const init = () => {
-  document.getElementById('root').innerHTML = "Working..."
+  document.getElementById('root').innerHTML = views.Header()
 }
 
 document.addEventListener('DOMContentLoaded', init)

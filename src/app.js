4af0e87862cfed5a02228c1ef6c9f3d2254c259f
github username <github email address>
2018-04-10 15:16:24 -0500

    Add responsive nav

--- src/app.js
+++ src/app.js
@@ -2,8 +2,26 @@ require('../node_modules/normalize.css/normalize.css')
 require('./css/style.css')
 import * as views from './views'
 
+const openSideMenu = () => {
+  document.getElementById('side-menu').style.height = '50%'
+  document.getElementById('side-menu').style.width = '300px'
+}
+
+const closeSideMenu = () => {
+  document.getElementById('side-menu').style.height = '0'
+  document.getElementById('side-menu').style.width = '0'
+}
+
 const init = () => {
   document.getElementById('root').innerHTML = views.Header()
+
+  const btnOpenSide = document.getElementById('btn-openSide')
+  btnOpenSide.addEventListener('click', openSideMenu)
+
+  const btnCloseSide = document.getElementById('btn-closeSide')
+  btnCloseSide.addEventListener('click', closeSideMenu)
 }
+
+
 
 document.addEventListener('DOMContentLoaded', init)
--- src/views.js
+++ src/views.js
@@ -2,11 +2,30 @@ export const Header = () => {
   return `
     <header>
       <div id=search>
+        <span class="open-slide">
+          <a href="#" id="btn-openSide">
+            <svg width="30" height="30">
+              <path d="M0,5 30,5" stroke="#3a3a3a" stroke-width="5"/>
+              <path d="M0,14 30,14" stroke="#3a3a3a" stroke-width="5"/>
+              <path d="M0,23 30,23" stroke="#3a3a3a" stroke-width="5"/>
+            </svg>
+          </a>
+        </span>
         <h1><i class="fas fa-sun"></i> Weather App</h1>
-        <input type="text" id="city" placeholder="Enter City"/>
-        <input type="text" id="state" placeholder="Enter State"/>
-        <i class="fas fa-search" id="btn-search"></i>
+        <div id="main-search">
+          <input type="text" id="city" placeholder="Enter City"/>
+          <input type="text" id="state" placeholder="Enter State"/>
+          <a href="#" class="btn-search"><i class="fas fa-search"></i></a>
+        </div>
       </div>
     </header>
+    <div id="side-menu" class="side-nav">
+      <a href="#" id="btn-closeSide">&times;</a>
+      <div id="inputs">
+      <input type="text" id="city" placeholder="Enter City"/>
+      <input type="text" id="state" placeholder="Enter State"/>
+      <a href="#" class="btn-search"><i class="fas fa-search"></i> Search</a>
+      </div>
+    </div>
   `
 }

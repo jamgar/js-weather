1f75ab0dd827f572bb2c4983cda17c8e031ebc92
github username <github email address>
2018-04-12 10:53:25 -0500

    Added styling to current and forecast

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require('../node_modules/normalize.css/normalize.css')
 require('./css/style.css')
+
 import * as views from './views'
 import { weather } from './weather'
 
@@ -40,17 +41,6 @@ const displayWeather = (weather) => {
 
 const init = () => {
   getWeather()
-
-  // const root = document.getElementById('root')
-  // root.innerHTML = views.Header()
-  // root.innerHTML += views.Current(weather)
-  // root.innerHTML += views.Forecast(weather)
-
-  // const btnOpenSide = document.getElementById('btn-openSide')
-  // btnOpenSide.addEventListener('click', openSideMenu)
-  //
-  // const btnCloseSide = document.getElementById('btn-closeSide')
-  // btnCloseSide.addEventListener('click', closeSideMenu)
 }
 
 
--- src/views.js
+++ src/views.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 const degToDirection = (num) => {
     var val = Math.floor((num / 22.5) + 0.5);
     var arr = ["N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"];
@@ -37,14 +39,21 @@ export const Header = () => {
 }
 
 export const Current = (weather) => {
+  // Get current days data
   const current = weather.list[0]
+  // Format date with moment
+  const currentDay = moment(Date(current.dt)).format('ddd')
+  const currentDate = moment(Date(current.dt)).format('MMM Do YY')
+
   return `
     <div id="current">
-      <h1 id="location">${weather.city.name} Weather Today</h1>
-      <h4>${Date(current.dt)}</h4>
+      <h1 id="location">${weather.city.name} Today</h1>
+      <h4>${currentDay} ${currentDate}</h4>
       <h3 id="description">${current.weather[0].description.toUpperCase()}</h3>
-      <h3 id="temp-string">${Math.floor(current.temp.day)} F</h3>
-      <img id="weather-icon" src="http://openweathermap.org/img/w/${current.weather[0].icon}.png" alt="current weather"/>
+      <div class="temp-desc">
+        <h3 id="temp-string">${Math.floor(current.temp.day)} F</h3>
+        <img id="weather-icon" src="http://openweathermap.org/img/w/${current.weather[0].icon}.png" alt="current weather"/>
+      </div>
       <ul id="details">
         <li id="humidity">Relative Humidity: ${current.humidity}%</li>
         <li id="wind">Wind: ${Math.floor(current.speed)}mph ${degToDirection(current.deg)}</li>
@@ -54,12 +63,21 @@ export const Current = (weather) => {
 }
 
 export const Forecast = (weather) => {
-  const forecast = weather.list.slice(1).map((item) => {
+  // Remove current day from array
+  weather = weather.list.slice(1)
+
+  const forecast = weather.map((item, index) => {
+    // Format date with moment
+    let forecastDay = moment().add(index + 1, 'days').format('ddd')
+    let forecastDate = moment().add(index + 1, 'days').format('MMM Do YY')
+
     return `
       <div class="forecast-day">
-        <h4>${Date(item.dt)}</h4>
-        <img id="forecast-icon" src="http://openweathermap.org/img/w/${item.weather[0].icon}.png" alt="forecast weather"/>
-        <p class="forecast-temp">${Math.floor(item.temp.day)} F</p>
+        <h4>${forecastDay} ${forecastDate}</h4>
+        <div class="temp-desc">
+          <p class="forecast-temp">${Math.floor(item.temp.day)} F</p>
+          <img class="forecast-icon" src="http://openweathermap.org/img/w/${item.weather[0].icon}.png" alt="forecast weather"/>
+        </div>
       </div>
       `
   }).join('')
